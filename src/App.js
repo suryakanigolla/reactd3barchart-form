2b9af735d461b23eee3417676759891c1bd252eb
Surya <suryakanigolla@gmail.com>
2022-05-19 23:47:05 +0530

    Added FormGroup Component, Styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import FormGroup from "components/FormGroup";
 
 import "./App.scss";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FormGroup text="Email" name="email" inputType="text" />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/FormGroup.js
@@ -0,0 +1,44 @@
+import React from "react";
+import classNames from "classnames";
+
+import styles from "./FormGroup.module.scss";
+
+const FormGroup = ({
+  text,
+  name,
+  inputType,
+  value,
+  onChange,
+  isInvalid,
+  isValid,
+  invalidFeedback,
+}) => {
+  const inputClassname = classNames({
+    [`${styles["form-group__input"]}`]: true,
+    [`${styles["form-group__input--valid"]}`]: isValid,
+    [`${styles["form-group__input--invalid"]}`]: isInvalid,
+  });
+
+  const feedbackClassname = classNames({
+    [`${"form-group__feedback"}`]: true,
+    [`${"form-group__feedback--invalid"}`]: isInvalid,
+  });
+
+  return (
+    <div className={styles["form-group"]}>
+      <label htmlFor={name} className={styles["form-group__label"]}>
+        {text}
+      </label>
+      <input
+        name={name}
+        className={inputClassname}
+        value={value}
+        onChange={onChange}
+        type={inputType}
+      />
+      <div className={styles[feedbackClassname]}>{invalidFeedback}</div>
+    </div>
+  );
+};
+
+export default FormGroup;
