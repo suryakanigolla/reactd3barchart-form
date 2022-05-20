795c39ff2c2f080ed55c93806a88885137c5f698
Surya <suryakanigolla@gmail.com>
2022-05-20 17:15:25 +0530

    Updated Email Regex

--- src/constants/regexHelpers.js
+++ src/constants/regexHelpers.js
@@ -1,2 +1,4 @@
-export const validEmail = new RegExp("^[a-z0-9]+@[a-z]+.[a-z]{2,3}$");
+export const validEmail = new RegExp(
+  "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+);
 export const validPhone = new RegExp("^[6-9]\\d{9}$");
